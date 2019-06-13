ccf74af7775d1888d4439e8b58522208dc44d4ea
GerogeLeon <leongeroge@gmail.com>
2019-06-13 17:33:06 +0800

    transfered here

---
+++ src/main/java/com/practice/spring/security/SecurityApplication.java
@@ -0,0 +1,17 @@
+package com.practice.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Luo Bao Ding
+ * @since 2018/5/23
+ */
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+
+}
